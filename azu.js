75c854a52b34410c737f86919d55df6485fb94dc
azmath2762 <70561336+azmath2762@users.noreply.github.com>
2020-09-05 17:25:58 +0530

    Add files via upload

---
+++ azu.js
@@ -0,0 +1,30 @@
+// // Program to find particular character in a string 
+// let str = "mangio"
+// console.log(str)
+// console.log(str.indexOf(str));
+// console.log(str.charAt(str))
+
+// // Q2. Program to convert minutes to seconds
+
+// let minitues=60;
+// console.log(minitues*60+" seconds");
+
+// Q3. Program to search element in a array of strings
+
+let name = [34,1,22,3,56,78,87]
+console.log(name);
+
+
+// Program to display only elements containing 'a' in the term of array
+
+// let name = ["apple","anar","mango","nun"]
+//  console.log(name)
+   
+//  console.log()
+
+//  Print an array in reverse order
+
+// let name = ["apple","anar","mango","nun"]
+// console.log(name)
+//  name = ["apple","anar","mango","nun"].reverse()
+// console.log((name))
